c85d325caa285db07b603cf29ba763efae261410
Arushi Mittal <arushimittal2432@gmail.com>
2023-11-18 10:30:41 +0530

    multiplication and divide

--- test/calc.c
+++ test/calc.c
@@ -4,7 +4,7 @@ int main()
 	int o;
 	printf("Enter the two numbers\n");
 	scanf("%f" "%f",&a,&b);
-	printf("Enter 1 to add , 2 to subtract ,3 to multiply and 4 to divide\n");
+	printf("Enter 1 to add ,2 to multiply and 3 to divide\n");
 	scanf("%d",&o);
 	
 	switch(o)
@@ -12,6 +12,23 @@ int main()
 		case 1:
 			f=(a+b);
 			printf("The sum of the two number is %f",f);
-			break; }
-    return 0;
+			break; 
+   
+		
+		case 2:
+			f=(a*b);
+			printf("The product of the two number is %f",f);
+			break;
+		case 3:
+			f=(a)/b;
+			printf("The result after division of the two number is %f",f);
+			break;
+	
+	
+
+	return 0;
 }
+
+}
+
+
